9dd8da8f22f8ed401d9001fda24f6d651ec82291
Ayesha Mosaddeque <ayesha@Ayeshas-MBP.gateway>
2018-09-27 21:06:13 +1000

    initial commit.

---
+++ spring-data-with-redis/src/main/java/com/reactivespring/demo/Game.java
@@ -0,0 +1,20 @@
+package com.reactivespring.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash
+public class Game implements Serializable {
+
+    @Id
+    private String id;
+    private String name;
+}
---
+++ spring-data-with-redis/src/main/java/com/reactivespring/demo/GameRepository.java
@@ -0,0 +1,8 @@
+package com.reactivespring.demo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends CrudRepository<Game, String> {
+}
