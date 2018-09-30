f76c08bb7eda6adac9988992a2a95c53cf542db5
ayeshasilvia <ayeshasilvia@gmail.com>
2018-09-30 18:48:33 +1000

    Adding some logs

--- spring-data-with-redis/src/main/java/com/reactivespring/demo/controller/GameTraditionalMVC.java
+++ spring-data-with-redis/src/main/java/com/reactivespring/demo/controller/GameTraditionalMVC.java
@@ -3,6 +3,7 @@ package com.reactivespring.demo.controller;
 import com.reactivespring.demo.GameService;
 import com.reactivespring.demo.model.Game;
 import com.reactivespring.demo.model.GameEvent;
+import lombok.extern.java.Log;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/games")
+@Log
 public class GameTraditionalMVC {
 
     private final GameService service;
@@ -24,18 +26,21 @@ public class GameTraditionalMVC {
     @GetMapping
     public Flux<Game> getAllgames()
     {
+        log.info("Received a request for games list");
         return this.service.getAllGames();
     }
 
     @GetMapping("/{id}")
     public Mono<Game> getGameById(@PathVariable String id)
     {
+        log.info("Received a request for a game with id: " + id);
         return this.service.getGameById(id);
     }
 
     @GetMapping(value = "/{id}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<GameEvent> getGameEvents(@PathVariable String id)
     {
+        log.info("Received a request for events with id: " + id);
         return this.service.getEvents(id);
     }
 
